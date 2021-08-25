a53b93c4ce65447a8420b7e506308265089d4b38
nxet <pnakml@gmail.com>
2021-08-25 17:53:17 +0200

    job.status: print matches as they're found

--- algovanity/__init__.py
+++ algovanity/__init__.py
@@ -16,6 +16,8 @@ class AlgoVanity:
     patterns = None
     procs_max = 1
 
+    matches = []
+
     def __init__(self, patterns, procs_max=None, debug=None, logger=None):
         self._debug = debug if debug is not None else self._debug
         self._logger = logger if logger is not None else self._logger
@@ -28,7 +30,10 @@ class AlgoVanity:
         self._queue_matches = Queue()
         self._procs = []
 
-    def run(self):
+    def run(self, debug=None, logger=None):
+        debug = debug if debug is not None else self._debug
+        logger = logger if logger is not None else self._logger
+        self.matches = []
         self._time_start = time()
         for i in range(self.procs_max):
             p = Process(
@@ -46,25 +51,36 @@ class AlgoVanity:
         try:
             while True:
                 sleep(5)
-                self._job_status()
+                # check for new matches, if available print them
+                matches = self._matches_pull_from_queue(self._queue_matches, debug=debug, logger=logger)
+                if matches:
+                    print('\r', end='') # deletes status line
+                    for match in matches:
+                        self.matches.append(match)
+                        print(*match) # pattern, where, address, pk
+                # print status line
+                with self._counter_attempts.get_lock():
+                    print(f'\r{round(time()-self._time_start)}s - {self._queue_matches.qsize() + len(self.matches)} matches in {self._counter_attempts.value} attempts', end='')
         except KeyboardInterrupt:
             print('')
-            self._job_terminate()
+            self._job_terminate(debug=debug, logger=logger)
 
-    def _job_status(self):
-        with self._counter_attempts.get_lock():
-            print(f'\r{round(time()-self._time_start)}s - {self._queue_matches.qsize()} matches in {self._counter_attempts.value} attempts', end='')
-
-    def _job_terminate(self):
+    def _job_terminate(self, debug=None, logger=None):
+        debug = debug if debug is not None else self._debug
+        logger = logger if logger is not None else self._logger
         for proc in self._procs:
             if proc.is_alive():
                 proc.terminate()
-        self.matches = []
-        while not self._queue_matches.empty():
-            #pattern, where, address, pk = self._queue_matches.get()
-            match = self._queue_matches.get()
-            self.matches.append(match)
+        self.matches += self._matches_pull_from_queue(self._queue_matches, debug=debug, logger=logger)
         return True
+
+    @classmethod
+    def _matches_pull_from_queue(cls, queue, debug=False, logger=None):
+        matches = []
+        while not queue.empty():
+            match = queue.get()
+            matches.append(match)
+        return matches
 
 
     def load_patterns(self, patterns, debug=None, logger=None):
