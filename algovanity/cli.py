1c7d75fc328a91984a54cb5f4cd60206525e7a90
nxet <pnakml@gmail.com>
2021-08-25 18:41:18 +0200

    job.run.output

--- algovanity/cli.py
+++ algovanity/cli.py
@@ -12,11 +12,12 @@ def main():
     parser = ArgumentParser(__pkgname__, formatter_class=RawTextHelpFormatter)
     parser.add_argument('PATTERNS', type=str, nargs='+', help='see readme')
     parser.add_argument('-p', '--procs-max', dest='PROCS_MAX', type=int, default=cpu_count(), help='maximum amount of concurrent processes to run, defaults to CPU number')
+    parser.add_argument('-o', '--output', dest='OUTPUT', type=str, help='path to output file - if file exists the new results will be appended')
     parser.add_argument('--debug', const=True, dest='DEBUG', action='store_const', default=False)
     args = parser.parse_args()
     logger = None #get_logger(__pkgname__, args.LOGLEVEL)
     client = AlgoVanity(patterns=args.PATTERNS, procs_max=args.PROCS_MAX, debug=args.DEBUG, logger=logger)
-    client.run()
+    client.run(output=args.OUTPUT)
     from pprint import pprint; pprint(client.matches)
 
 
