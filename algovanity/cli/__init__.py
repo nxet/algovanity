a5125755f8f3fd78369aa0cd804920d33b1c6c9c
nxet <pnakml@gmail.com>
2021-08-26 13:16:45 +0200

    cli.logger

--- algovanity/cli/__init__.py
+++ algovanity/cli/__init__.py
@@ -3,12 +3,13 @@
 from algovanity.lib import AlgoVanity
 from algovanity.__metadata__ import __pkgname__, __version__
 from algovanity.cli.parser import get_parser
+from algovanity.cli.logger import get_logger
 
 
 def main():
     parser = get_parser(__pkgname__, __version__)
     args = parser.parse_args()
-    logger = None #get_logger(__pkgname__, args.LOGLEVEL)
+    logger = get_logger(__pkgname__, args.LOGLEVEL)
     client = AlgoVanity(patterns=args.PATTERNS, procs_max=args.PROCS_MAX, debug=args.DEBUG, logger=logger)
     client.run(output=args.OUTPUT)
 
